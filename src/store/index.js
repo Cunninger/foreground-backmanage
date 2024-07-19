18d7503660adff89d3c99a2961a00024f0a3153d
Cunninger <1803912219@qq.com>
2024-07-19 13:57:31 +0800

    Fix: 修复图片阴影

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,29 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
+
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    user: null,
-    // User(sysRole=SysRole(name=admin, description=管理员, available=true),
-    // username=金陵易和, password=123, lastLoginTime=null, status=true)
-  },
-  mutations: {
-    setUser(state, user) {
-      state.user = user;
-    }
-  },
-  actions: {
-    loginUser({ commit }, user) {
-      commit('setUser', user);
+    state: {
+        avatarUrl: '',
+        user: null,
+        // User(sysRole=SysRole(name=admin, description=管理员, available=true),
+        // username=金陵易和, password=123, lastLoginTime=null, status=true)
     },
+    mutations: {
+        setUser(state, user) {
+            state.user = user;
+        },
+        updateAvatarUrl(state, url) {
+            state.avatarUrl = url;
+        }
+    },
+    actions: {
+        loginUser({commit}, user) {
+            commit('setUser', user);
+        },
 
-  },
-  plugins: [createPersistedState()],
+    },
+    plugins: [createPersistedState()],
 });
